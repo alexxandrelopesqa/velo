c76d1af7139018abf91db19e8181f9c551543f26
alexxandrelopesqa <alexandre.lopes@academico.ifpb.edu.br>
2026-03-17 12:34:29 -0300

    AAA - Arrange, Act, Assert

--- e2e/online.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test' 
-
-test('Deve validar o título da página', async ({ page }) => {
-  await page.goto('http://localhost:5173/') 
-
-  // Checkpoint
-  await expect(page).toHaveTitle(/Velô by Alexandre QA Engineer/) 
-}) 
--- e2e/pedidos.spec.ts
+++ e2e/pedidos.spec.ts
@@ -1,20 +1,23 @@
-import { test, expect } from '@playwright/test' 
+import { test, expect } from '@playwright/test'
+
+// AAA - Arrange, Act, Assert
 
 test('Deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/') 
-
-  // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint') 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click() 
 
-  // Checkpoint
+  // Act
   await page.getByTestId('search-order-id').click() 
   await page.getByTestId('search-order-id').fill('VLO-MVK05A') 
   await page.getByTestId('search-order-button').click() 
+
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible() 
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-MVK05A') 
 
-  // Checkpoint
   await expect(page.getByTestId('order-result-status')).toBeVisible()  
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')  
+
 }) 
